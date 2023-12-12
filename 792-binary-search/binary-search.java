63ee69fd4325f9382396ffdac184369c3b5ad113
belovedbb-trunc <belovedbb1@yahoo.com>
2023-12-12 19:02:02 +0100

    Time: 0 ms (100.00%) | Memory: 44.8 MB (55.74%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while( high >= low) {
+            int mid = (high + low)/2;
+            if(target < nums[mid]) 
+                high = mid - 1;
+            else if(target == nums[mid])
+                return mid;
+            else
+                low = mid  + 1;
+        }
+        return -1;
+    }
+}
