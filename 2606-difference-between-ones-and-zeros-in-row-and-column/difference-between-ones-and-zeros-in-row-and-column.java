05bfd56c9056b3887a1059c993d702aed40da682
belovedbb-trunc <belovedbb1@yahoo.com>
2023-12-14 11:51:58 +0100

    Time: 10 ms (61.81%) | Memory: 71.3 MB (82.20%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
@@ -3,36 +3,27 @@ class Solution {
     Map<Integer, Integer> col = new HashMap<>();
     public int[][] onesMinusZeros(int[][] grid) {
         int[][] res = new int[grid.length][grid[0].length];
+        int[] row = new int[grid.length];
+        int[] col = new int[grid[0].length];
         for(int i = 0; i < grid.length; i++) {
-            if(!row.containsKey(i)) {
-                row.put(i, getOneRow(grid[i]));
-            }
-            int oneR = row.get(i);
-            int zeroR = grid[0].length - oneR;
             for(int j = 0; j < grid[i].length; j++) {
-                if(!col.containsKey(j)) {
-                    col.put(j, getOneCol(grid, j));
+                if(grid[i][j] == 1) {
+                    row[i] += grid[i][j];
+                    col[j] += grid[i][j];
                 }
-                int oneC = col.get(j);
-                int zeroC = grid.length - oneC;
-                res[i][j] = (oneR + oneC - zeroR - zeroC);
+            }
+        }
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                int oneRow = row[i];
+                int zeroRow = grid.length - oneRow;
+                int oneCol = col[j];
+                int zeroCol = grid[0].length - oneCol;
+                res[i][j] = oneRow + oneCol - zeroRow - zeroCol;
             }
         }
         return res;
     }
 
-    int getOneRow(int[] r) {
-        int count = 0;
-        for(int i = 0; i < r.length; i++) 
-            if(r[i] == 1) count++;
-        return count;
-    }
 
-    int getOneCol(int[][] m, int c) {
-        int count = 0;
-        for(int i = 0; i < m.length; i++) {
-            if(m[i][c] == 1)  count++;
-        }
-        return count;
-    }
 }
