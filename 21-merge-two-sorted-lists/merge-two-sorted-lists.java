e27247bcd6ea5ef1bc30a05fa428873928cb2dbc
belovedbb-trunc <belovedbb1@yahoo.com>
2025-05-27 13:17:15 +0300

    Time: 2 ms (2.71%) | Memory: 43.2 MB (8.53%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,43 +10,36 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null && list2 == null) return null;
-        List<Integer> m = collect(list1, list2);
-        ListNode res = new ListNode();
-        ListNode temp = res;
-        merge(temp, m);
-        return res;
-    }
-
-    List<Integer> collect(ListNode list1, ListNode list2) {
         List<Integer> m = new ArrayList<>();
         while(list1 != null && list2 != null) {
             m.add(list1.val);
             m.add(list2.val);
-
             list1 = list1.next;
+            list2 = list2.next;
+        }
+        while(list2 != null) {
+            m.add(list2.val);
             list2 = list2.next;
         }
         while(list1 != null) {
             m.add(list1.val);
             list1 = list1.next;
         }
-        while(list2 != null) {
-            m.add(list2.val);
-            list2 = list2.next;
-        }
         Collections.sort(m);
-        return m;
-    }
-
-    void merge(ListNode res, List<Integer> m) {
+        ListNode node1 = null;
+        ListNode node = null;
         for(int i = 0; i < m.size(); i++) {
-            res.val = m.get(i);
-            if(i != m.size() - 1) {
-                res.next = new ListNode();
+            Integer val = m.get(i);
+            if(node1 == null) {
+                node1 =new ListNode();
+                node = node1;
             }
-            res = res.next;
-        }   
+            node.val = val;
+            if(i  != m.size() - 1) {
+                node.next = new ListNode();   
+                node = node.next;
+            }
+        }
+        return node1;
     }
-
 }
